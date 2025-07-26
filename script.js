2d0d6ab26aae1b267d770ae0f2ea370a229bf65d
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-07-26 15:07:55 +0700

    Books displayed on page

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-let hamster = {
-    stomach: [],
+const myLibrary = [];
 
-    eat(food) {
-        // this.stomach.push(food);
-        this.stomach = [food]
-    },
-};
+function Book(title, author, pageNumber, isRead) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
 
-let speedy = {
-    __proto__: hamster,
-    // stomach: [],
-};
+    this.id = crypto.randomUUID();
 
-let lazy = {
-    __proto__: hamster,
-    // stomach: [],
-};
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.isRead = isRead;
 
-// This one found the food
-speedy.eat("apple");
-console.log(speedy.stomach); // apple
+    const pageInfo = pageNumber > 1 ? "pages" : "page";
 
-// This one also has it, why? fix please.
-console.log(lazy.stomach); // apple
+    this.info = function () {
+        // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+
+        const isReadInfo = isRead ? "has read" : "not read yet";
+
+        return `${title} by ${author}, ${pageNumber} ${pageInfo}, ${isReadInfo}`;
+    };
+
+    this.display = function () {
+        return [title, author, `${pageNumber} ${pageInfo}`];
+    };
+}
+
+function addBookToLibrary(title, author, pageNumber, isRead) {
+    myLibrary.push(new Book(title, author, pageNumber, isRead));
+}
+
+function displayLibrary() {
+    const container = document.querySelector(".container");
+
+    for (const book of myLibrary) {
+        const card = document.createElement("ul");
+
+        card.classList.toggle("book");
+        if (book.isRead) {
+            card.classList.toggle("read");
+        }
+
+        for (const prop of book.display()) {
+            const info = document.createElement("li");
+            info.textContent = prop;
+            card.appendChild(info);
+        }
+
+        container.appendChild(card);
+    }
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+addBookToLibrary("Winnie-the-Pooh", "A. A. Milne", 176, true);
+
+displayLibrary();
